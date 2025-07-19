b7e81fefef50be703a5c2ed1a928a5472ccfb1c1
D-a-k-s-h <daksh.kumar.ug22@gmail.com>
2025-07-19 15:32:20 +0530

    refactor: Update environment configurations and API base URL for production

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://bankyou-sewa-kendra-backend.onrender.com/api/v1";
 
 export const otpEndpoint = {
     SENDOTP_API: BASE_URL + '/auth/sendotp',
